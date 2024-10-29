ddd0a6fa37ff3eb8d2274da8a1ee2e54b5baa9a5
pramodbirla01 <prmdbirla65@gmail.com>
2024-10-29 15:22:35 +0530

    cart

---
+++ src/pages/cart/index.js
@@ -0,0 +1,12 @@
+import Nav from "@/component/Nav";
+import Cart from "./Cart";
+import Footer from "@/component/Footer";
+export default function cart(params) {
+    return(
+        <>
+        <Nav/>
+        <Cart/>
+        <Footer/>
+        </>
+    );
+}
